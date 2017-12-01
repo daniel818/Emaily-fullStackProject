23ef16643d8be6a48fa0b5a0d279c60ae6165e44
daniel818 <avital.daniel@gmail.com>
2017-12-01 18:25:34 +0200

    adding surveys routes

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,11 +1,23 @@
 /**
  * Created by Daniel on 30/11/2017.
  */
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 
+const Survey = mongoose.model("surveys");
+
 module.exports = app => {
   app.post("/api/surveys", requireLogin, requireCredits, (req, user) => {
+    const { title, subject, body, recipients } = req.body;
 
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
   });
 };
